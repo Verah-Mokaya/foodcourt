6df101342545ed0ec9c14b745a2870bf92aa3a58
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-28 18:29:02 +0300

    added login page to check if both email n hashed password are correct

--- server/routes/outlet_auth.py
+++ server/routes/outlet_auth.py
@@ -29,3 +29,15 @@ def register_outlet():
                     "outlet_id": new_outlet.id
                     }), 201
     
+
+@outlet_auth.route('/api/outlet/login', methods=['POST'])
+def login_outlet():
+    data = request.get_json()
+    outlet = Outlet.query.filter_by(email=data['email']).first()
+
+    if outlet and bcrypt.check_password_hash(outlet.password, data['password']):
+        return jsonify({'message': 'Login successful',
+                        'outlet_id': outlet.id
+                        }), 200
+    else:
+        return jsonify({'message': 'Invalid email or password'}), 401
