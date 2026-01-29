8590fe6a063e9fb130ef91e69c2a67a88cdc1868
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-29 15:15:58 +0300

    added app configuation

---
+++ server/app.py
@@ -0,0 +1,6 @@
+import os
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://foodcourt_user:password@localhost:5432/foodcourt_db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-key')
+    
