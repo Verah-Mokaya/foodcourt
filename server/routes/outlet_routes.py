be9d6e64d14b4d4e16583c22c7b49758d4771e90
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-29 12:00:58 +0300

    add get outlet profile endpoint

--- server/routes/outlet_routes.py
+++ server/routes/outlet_routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, app
 from routes.outlet_routes import outlet_bp
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 outlet_bp=Blueprint('outlet', __name__)
 
 app.register_blueprint(outlet_bp, url_prefix='/outlets')
 
+def outlet_only():
+    claims = get_jwt()
+    if claims.get("role") != "outlet":
+        return False
+    return True
