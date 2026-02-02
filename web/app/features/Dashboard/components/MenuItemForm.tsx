7d3fe914d16fe6ea10193c65287cea893e89280d
Liban Golo <liban.golo@student.moringaschool.com>
2026-02-02 03:12:02 +0300

    add OrdersProps interface

--- web/app/features/Dashboard/components/MenuItemForm.tsx
+++ web/app/features/Dashboard/components/MenuItemForm.tsx
@@ -2,3 +2,8 @@
 
 import { Order } from "@/lib/types";
 import { Check, ChefHat, Clock } from "lucide-react";
+
+interface OrdersProps {
+    orders: Order[];
+    updateStatus: (id: number, status: string) => void;
+}
