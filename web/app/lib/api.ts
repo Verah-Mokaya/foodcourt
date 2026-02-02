62419d1ac069b803f47715e0ee57bced2b11ce46
gitahisamuel <murimisamuel05@gmail.com>
2026-02-02 18:07:47 +0300

    safely access auth token in browser environment

--- web/app/lib/api.ts
+++ web/app/lib/api.ts
@@ -2,3 +2,7 @@ export const API_URL =
     process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export const fetcher = async <T>(url: string): Promise<T> => {
+      const token =
+        typeof window !== "undefined"
+            ? localStorage.getItem("fc_token")
+            : null;
