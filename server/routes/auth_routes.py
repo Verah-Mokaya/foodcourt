81148be842f44794e2e56753dd1bd7523223254c
Verah Mokaya <verahmokaya475@gmail.com>
2026-02-05 14:06:38 +0300

    imported jwt cookies to auth route and allowed cookies are cleared upon logout

--- server/routes/auth_routes.py
+++ server/routes/auth_routes.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import (
     create_access_token,
     jwt_required,
-    get_jwt_identity
+    get_jwt_identity,
+    unset_jwt_cookies
 )
 
 from extensions import db, bcrypt
@@ -237,7 +238,9 @@ def reset_password():
 @auth_bp.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
-
-    return jsonify({
+    response = jsonify({
         "message": "Logout successful. Delete token on client."
-    }), 200
+    })
+    unset_jwt_cookies(response)
+
+    return response, 200
