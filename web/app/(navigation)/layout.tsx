3c2f9a16afc50a5fd6ffa3f5ac9f0adfbe251d56
Liban Golo <liban.golo@student.moringaschool.com>
2026-02-09 23:24:27 +0300

    Import Navbar, Footer, and ROUTES

---
+++ web/app/(navigation)/layout.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import { usePathname } from "next/navigation";
+import { ROUTES } from "@/app/lib/routes";
