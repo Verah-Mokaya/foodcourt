1d9396cbfb4e23582fe50b68fca4d7e03f42e356
Liban Golo <liban.golo@student.moringaschool.com>
2026-02-09 16:37:18 +0300

    Add DashboardHomeProps interface

--- web/app/(navigation)/dashboard/components/DashboardHome.tsx
+++ web/app/(navigation)/dashboard/components/DashboardHome.tsx
@@ -1,3 +1,11 @@
 "use client";
 
 import { Check, ChefHat, Clock } from "lucide-react";
+
+interface DashboardHomeProps {
+    stats: {
+        pending: number;
+        preparing: number;
+        ready: number;
+    }
+}
