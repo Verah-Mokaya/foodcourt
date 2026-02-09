9a8be8087b6a972497be91496dab53658c5ad69f
Newton Oduor <newtonochieng@gmail.com>
2026-02-09 23:27:42 +0300

    Add basic navigation layout with Navbar and Footer components

---
+++ web/app/(navigation)/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Navbar from "@/app/components/Navbar";
+import Footer from "@/app/components/Footer";
+import { usePathname } from "next/navigation";
+import { ROUTES } from "@/app/lib/routes";
+
