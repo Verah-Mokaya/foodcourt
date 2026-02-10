e79642f5a17c75ff143d79599b845fa3f07458d6
Susan Gakii <susangakii71@gmail.com>
2026-02-10 08:51:17 +0300

    updates seed data

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,4 @@
-from app import create_app
-app = create_app()
+from app import app
 from models import db, Customer, Outlet, MenuItem, Order, OrderItem, Reservation, FoodCourtTable
 from faker import Faker
 from random import randint, choice, uniform
